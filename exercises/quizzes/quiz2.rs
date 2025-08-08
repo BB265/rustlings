c24979ca9a10ed495edf9ea35f3b820dae92a525
BB265 <3230103612@zju.edu.cn>
2025-08-08 12:52:19 +0800

    Exercises Completed by BB265

---
+++ exercises/quizzes/quiz2.rs
@@ -0,0 +1,108 @@
+// This is a quiz for the following sections:
+// - Strings
+// - Vecs
+// - Move semantics
+// - Modules
+// - Enums
+//
+// Let's build a little machine in the form of a function. As input, we're going
+// to give a list of strings and commands. These commands determine what action
+// is going to be applied to the string. It can either be:
+// - Uppercase the string
+// - Trim the string
+// - Append "bar" to the string a specified amount of times
+//
+// The exact form of this will be:
+// - The input is going to be a Vector of 2-length tuples,
+//   the first element is the string, the second one is the command.
+// - The output element is going to be a vector of strings.
+
+enum Command {
+    Uppercase,
+    Trim,
+    Append(usize),
+}
+
+mod my_module {
+    use super::Command;
+
+    // TODO: Complete the function as described above.
+    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        // Implement 1
+        let mut output: Vec<String> = vec![];
+        for pi in input {
+            let mut po: String;
+            match pi.1 {
+                Command::Uppercase => po = pi.0.to_uppercase(),
+                Command::Trim => po = pi.0.trim().to_string(),
+                // Command::Append(times) => {
+                //     po = pi.0;
+                //     for _ in 0..times {
+                //         po = po.to_string() + "bar";
+                //     }
+                // }
+                Command::Append(times) => {
+                    let mut s = pi.0;
+                    for _ in 0..times {
+                        s.push_str("bar");
+                    }
+                    po = s;
+                }
+            }
+            output.push(po);
+        }
+        output
+
+
+        // Implement 2
+        // input
+        //     .into_iter()
+        //     .map(|(string, commmand)| {
+        //         match commmand {
+        //             Command::Uppercase => string.to_uppercase(),
+        //             Command::Trim => string.trim().to_string(),
+        //             Command::Append(times) => {
+        //                 let mut s = string;
+        //                 s.push_str(&"bar".repeat(times));
+        //                 s
+        //             }
+        //         }
+        //     })
+        //     .collect()
+    }
+    
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO: What do we need to import to have `transformer` in scope?
+    // use ???;
+    use super::my_module::transformer;
+    use super::Command;
+
+    #[test]
+    fn it_works() {
+        let input = vec![
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
+        ];
+        let output = transformer(input);
+
+        assert_eq!(
+            output,
+            [
+                "HELLO",
+                "all roads lead to rome!",
+                "foobar",
+                "barbarbarbarbarbar",
+            ]
+        );
+    }
+}
